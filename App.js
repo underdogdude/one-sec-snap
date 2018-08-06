0b4adf805305586391868b85c83f7b5f6f4b7c5a
underdogdude <underdogbright@gmail.com>
2018-08-06 15:57:30 +0700

    first commit for one sec snap

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>One Sec Snap</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title : {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#444'
+  }
+});
